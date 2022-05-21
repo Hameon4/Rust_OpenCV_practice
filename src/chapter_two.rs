84652f12e167520bc939598004cea394f70c3eff
Hamad H. Alsheraifi <61793446+Hameon4@users.noreply.github.com>
2022-05-21 22:00:17 +0400

    Update chapter_two.rs

--- src/chapter_two.rs
+++ src/chapter_two.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use opencv:: {
     prelude::*,
+    core,
     highgui,
     imgproc,
     imgcodecs
@@ -12,14 +13,24 @@ pub fn func() -> Result<()> {
     
     // Converting to Gray
     let mut img_gray = Mat::default();  // decalre a variable of Mat type to be used for conversion
-    imgproc::cvt_color(&img, &mut img_gray, imgproc::COLOR_BGR2GRAY, 0)?;   
+    imgproc::cvt_color(
+        &img,
+        &mut img_gray,
+        imgproc::COLOR_BGR2GRAY,
+        0)?;
 
     // Converting to Gaussian Blur 
     let mut img_blur = Mat::default();
-    imgproc::gaussian_blur(&img_gray, &mut img_blur, Size, 5.0, 0.0, 0)?;
+    imgproc::gaussian_blur(
+        &img, &mut img_blur,
+        core::Size::from((7, 7)),
+        5.0,
+        0.0,
+        0)?;
     
     highgui::imshow("Image", &img)?;
     highgui::imshow("Image Gray", &img_gray)?;
+    highgui::imshow("Image Blur", &img_blur)?;
     highgui::wait_key(0)?;
     Ok(())
 }
