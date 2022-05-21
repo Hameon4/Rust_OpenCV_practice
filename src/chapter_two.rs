f7ee06c8a8527698ff12d26478787145e7a9617c
Hamad H. Alsheraifi <61793446+Hameon4@users.noreply.github.com>
2022-05-21 22:28:43 +0400

    Update chapter_two.rs

--- src/chapter_two.rs
+++ src/chapter_two.rs
@@ -6,6 +6,7 @@ use opencv:: {
     imgproc,
     imgcodecs
 };
+use opencv::imgproc::MORPH_TOPHAT;
 
 pub fn func() -> Result<()> {
     let path = String::from("Resources/rust_wallpaper.jpg");
@@ -23,14 +24,43 @@ pub fn func() -> Result<()> {
     let mut img_blur = Mat::default();
     imgproc::gaussian_blur(
         &img, &mut img_blur,
-        core::Size::from((7, 7)),
-        5.0,
+        core::Size::from((3, 3)),
+        3.0,
         0.0,
-        0)?;
+        core::BORDER_DEFAULT)?;
+
+    // Edge Detection (Canny Edge Detector - Most Popular method)
+    // Usually when detecting edges, its source image is blurred
+    let mut img_canny = Mat::default();
+    imgproc::canny(
+        &img_blur,
+        &mut img_canny,
+        150.0,
+        250.0,
+        3,
+        false)?;
+
+    // Erosion (decrease thickness) and Dilation (increase thickness)
+    let mut img_dilation = Mat::default();
+    let mut img_erosion = Mat::default();
+    let kernel = imgproc::get_structuring_element(
+        imgproc::MORPH_RECT,
+        core::Size::from(5, 5),
+        core::Point::from(-1, -1))?;
+    imgproc::dilate(
+        &img_canny,
+        &mut img_dilation,
+        &kernel,
+        core::Point::from(-1, -1),
+        1,
+        core::BORDER_CONSTANT,
+        imgproc::morphology_default_border_value().unwrap())?;
     
     highgui::imshow("Image", &img)?;
     highgui::imshow("Image Gray", &img_gray)?;
     highgui::imshow("Image Blur", &img_blur)?;
+    highgui::imshow("Image Canny", &img_canny)?;
+    highgui::imshow("Image Dilation", &img_dilation)?;
     highgui::wait_key(0)?;
     Ok(())
 }
