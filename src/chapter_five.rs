f59989478b770f88030e40178cf67ba38d5b2bac
Hamad H. Alsheraifi <61793446+Hameon4@users.noreply.github.com>
2022-06-05 12:01:13 +0400

    Update chapter_five.rs

--- src/chapter_five.rs
+++ src/chapter_five.rs
@@ -1,3 +1,4 @@
+use std::ops::Add;
 use anyhow::Result;
 use opencv:: {
     prelude::*,
@@ -11,10 +12,22 @@ pub fn warp_perspective() -> Result<()> {
     let path = String::from("Resources/cards.jpg");
     let img = imgcodecs::imread(&path, imgcodecs::IMREAD_COLOR)?;
 
-    let src = core::Point2f::from((529, 142), (771, 190), (405, 395), (674, 457));  // error
-    let w = 250;
-    let h = 350;
-    let dst = core::Point2f::from((0.0, 0.0), (w, 0.0), (0.0, h), (w, h)); // error
+    // (529, 142), (771.0, 190.0), (405.0, 395.0), (674.0, 457.0)
+    // let arr1: [[f32; 2]; 4] = [[529.0, 142.0], [771.0, 190.0], [405.0, 395.0], [674.0, 457.0]];
+    let p1 = core::Point2f::from((529.0, 142.0));
+    let p2 = core::Point2f::from((771.0, 190.0));
+    let p3 = core::Point2f::from((405.0, 395.0));
+    let p4 = core::Point2f::from((674.0, 457.0));
+    let src = vec![p1, p2, p3, p4]; // error
+    let w = 250.0;
+    let h = 350.0;
+   // let dst = core::Point2f::from((0.0, 0.0), (w, 0.0), (0.0, h), (w, h)); // error
+    //let arr2: [[f32; 2]; 4] = [[0.0, 0.0], [w, 0.0], [0.0, h], [w, h]];
+    let q1 = core::Point2f::from((0.0, 0.0));
+    let q2 = core::Point2f::from((w, 0.0));
+    let q3 = core::Point2f::from((0.0, h));
+    let q4 = core::Point2f::from((w, h));
+    let dst = vec![q1, q2, q3, q4];
 
     let mut img_warp = Mat::default();
     let matrix = imgproc::get_perspective_transform(&src, &dst, core::DECOMP_LU)?;
