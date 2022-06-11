3df9f0c7e2211858c4ccfb955725287bdfe53564
Hamad H. Alsheraifi <61793446+Hameon4@users.noreply.github.com>
2022-06-11 09:24:09 +0400

    Update chapter_five.rs

--- src/chapter_five.rs
+++ src/chapter_five.rs
@@ -7,6 +7,7 @@ use opencv:: {
     highgui,
     core
 };
+use opencv::core::Point2f;
 
 pub fn warp_perspective() -> Result<()> {
     let path = String::from("Resources/cards.jpg");
@@ -14,21 +15,20 @@ pub fn warp_perspective() -> Result<()> {
 
     // (529, 142), (771.0, 190.0), (405.0, 395.0), (674.0, 457.0)
     // let arr1: [[f32; 2]; 4] = [[529.0, 142.0], [771.0, 190.0], [405.0, 395.0], [674.0, 457.0]];
-    let p1 = core::Point2f::from((529.0, 142.0));
-    let p2 = core::Point2f::from((771.0, 190.0));
-    let p3 = core::Point2f::from((405.0, 395.0));
-    let p4 = core::Point2f::from((674.0, 457.0));
-    let src= core:
+    let p1: Point2f = core::Point2f::from((529.0, 142.0));
+    let p2: Point2f = core::Point2f::from((771.0, 190.0));
+    let p3: Point2f = core::Point2f::from((405.0, 395.0));
+    let p4: Point2f = core::Point2f::from((674.0, 457.0));
+    let src: [[f64; 2]; 4] = [[529.0, 142.0], [771.0, 190.0], [405.0, 395.0], [674.0, 457.0]];
     let w = 250.0;
     let h = 350.0;
    // let dst = core::Point2f::from((0.0, 0.0), (w, 0.0), (0.0, h), (w, h)); // error
     //let arr2: [[f32; 2]; 4] = [[0.0, 0.0], [w, 0.0], [0.0, h], [w, h]];
-    let q1 = core::Point2f::from((0.0, 0.0));
-    let q2 = core::Point2f::from((w, 0.0));
-    let q3 = core::Point2f::from((0.0, h));
-    let q4 = core::Point2f::from((w, h));
-    let dst = Mat::default();
-    dst.add((q1, q2, q3, q4));
+    let q1: Point2f = core::Point2f::from((0.0, 0.0));
+    let q2: Point2f = core::Point2f::from((w, 0.0));
+    let q3: Point2f = core::Point2f::from((0.0, h));
+    let q4: Point2f = core::Point2f::from((w, h));
+    let dst:[[f32; 2]; 4] = [[0.0, 0.0], [w, 0.0], [0.0, h], [w, h]];
 
     let mut img_warp = Mat::default();
     let matrix = imgproc::get_perspective_transform(&src, &dst, core::DECOMP_LU)?;
