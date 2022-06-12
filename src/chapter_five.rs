5a69067fb28ee31aa629e159cfafad3f37bc33d7
Hamad H. Alsheraifi <61793446+Hameon4@users.noreply.github.com>
2022-06-12 14:06:18 +0400

    update

--- src/chapter_five.rs
+++ src/chapter_five.rs
@@ -12,29 +12,28 @@ pub fn warp_perspective() -> Result<()> {
     let path = String::from("Resources/cards.jpg");
     let img = imgcodecs::imread(&path, imgcodecs::IMREAD_COLOR)?;
 
-    // (529, 142), (771.0, 190.0), (405.0, 395.0), (674.0, 457.0)
-    // let arr1: [[f32; 2]; 4] = [[529.0, 142.0], [771.0, 190.0], [405.0, 395.0], [674.0, 457.0]];
     let v_src = vec![
         core::Point2f::from((529.0, 142.0)),
         core::Point2f::from((771.0, 190.0)),
         core::Point2f::from((405.0, 395.0)),
         core::Point2f::from((674.0, 457.0)),
     ];
-    let src = types::VectorOfPoint2f::from(v_src);
+
     let w = 250.0;
     let h = 350.0;
-   // let dst = core::Point2f::from((0.0, 0.0), (w, 0.0), (0.0, h), (w, h)); // error
-    //let arr2: [[f32; 2]; 4] = [[0.0, 0.0], [w, 0.0], [0.0, h], [w, h]];
+
     let v_dst = vec![
         core::Point2f::from((0.0, 0.0)),
         core::Point2f::from((w, 0.0)),
         core::Point2f::from((0.0, h)),
         core::Point2f::from((w, h)),
     ];
+
+    let src = types::VectorOfPoint2f::from(v_src);
     let dst = types::VectorOfPoint2f::from(v_dst);
 
     let mut img_warp = Mat::default();
-    let matrix = imgproc::get_perspective_transform(&src, &dst, core::DECOMP_LU);
+    let matrix = imgproc::get_perspective_transform(&src, &dst, core::DECOMP_LU)?;
     imgproc::warp_perspective(
         &img,
         &mut img_warp,
