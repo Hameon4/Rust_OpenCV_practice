cc4fb7eb42ccaa1491b37838e4fedb117f7a2399
Hamad H. Alsheraifi <61793446+Hameon4@users.noreply.github.com>
2022-06-14 10:23:11 +0400

    update

---
+++ src/chapter_six.rs
@@ -0,0 +1,13 @@
+use anyhow::Result;
+use opencv::{
+    imgcodecs,
+    highgui
+};
+
+pub fn color_detection() -> Result <()> {
+    let path = String::from("Resources/lambo.png");
+    let img = imgcodecs::imread(&path, imgcodecs::IMREAD_COLOR)?;
+    highgui::imshow("Image", &img)?;
+    highgui::wait_key(0)?;
+    Ok(())
+}
