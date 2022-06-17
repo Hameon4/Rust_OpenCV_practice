a74d6db55ca2976823827791a17a87aed55b15cb
Hamad H. Alsheraifi <61793446+Hameon4@users.noreply.github.com>
2022-06-17 10:45:57 +0400

    Update chapter_six.rs

--- src/chapter_six.rs
+++ src/chapter_six.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use opencv::{imgcodecs, highgui, imgproc, core, prelude::*, types};
+use opencv::{imgcodecs, highgui, imgproc, core, prelude::*};
 
 pub fn color_detection() -> Result <()> {
     let path = String::from("Resources/lambo.png");
@@ -7,14 +7,13 @@ pub fn color_detection() -> Result <()> {
 
     let mut img_hsv = Mat::default();
     let mut mask = Mat::default();
-    let hmin = 0; let smin = 110; let vmin = 153;
-    let hmax = 19; let smax = 240; let vmax = 255;
+    let hmin = 0.0; let smin = 110.0; let vmin = 153.0;
+    let hmax = 19.0; let smax = 240.0; let vmax = 255.0;
 
 
     imgproc::cvt_color( &img, &mut img_hsv, imgproc::COLOR_BGR2HSV, 0)?;
     // inrange used to collect the color
-    let v_lower = vec![core::Scalar::from((hmin))];
-    let lower = types::VectorOfScalar::from(v_lower);
+    let lower = core::Scalar::from((hmin, smin, vmin));
     let upper = core::Scalar::from((hmax, smax, vmax));
     core::in_range(&img_hsv, &lower, &upper, &mut mask)?;
 
